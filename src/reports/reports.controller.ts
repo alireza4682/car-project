bc99acb3d1a83ec2065ce63d0997ec4efb843a56
Alireza Arezoumandan <comolestyrusistantaneos@gmail.com>
2023-08-28 21:44:55 +0330

    reports many to one relation

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { ReportsService } from './reports.service';
+import { AuthGaurd } from 'src/gaurds/auth.gaurds';
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+  constructor(private reportsService: ReportsService) {}
+
+  @Post()
+  @UseGuards(AuthGaurd)
+  createReport(@Body() body: CreateReportDto) {
+    return this.reportsService.create(body);
+  }
+}
